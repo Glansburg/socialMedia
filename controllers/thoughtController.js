dca0786c9e2ec42b342b9245790813367f212368
Joel Getzke <joelcode2023@gmail.com>
2023-09-12 17:00:20 -0500

    added update/remove controller for thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -40,4 +40,37 @@ module.exports = {
         }
       },
 
+  // Delete a thought and remove them from the database
+async deleteThought(req, res) {
+  try {
+    const thought = await Thought.findOneAndRemove({ _id: req.params.thoughtId });
+
+    if (!thought) {
+      return res.status(404).json({ message: 'No such thought exists' });
+    }
+    res.json({ message: 'Thought successfully deleted' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+},
+// Update a thought
+async updateThought(req, res) {
+  try {
+    const thought = await Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId},
+      { $set: req.body },
+      { runValidators: true, new: true }
+    );
+
+    if (!thought) {
+      res.status(404).json({ message: 'No thought with this id!' });
+    }
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+},
+
 }
